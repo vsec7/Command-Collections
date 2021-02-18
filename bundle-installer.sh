a7222f8f70629ed939567529577020f00eebde7f
Viloid <vsec48@gmail.com>
2021-02-18 12:04:23 +0700

    Add some awesome tools :)

--- bundle-installer.sh
+++ bundle-installer.sh
@@ -11,9 +11,17 @@ cat <<EOF
 +------------------------------------------------------------------+
 EOF
 
+cd /tmp
+
 echo "[+] Installing Findomain (@Findomain)"
 sudo wget https://github.com/Findomain/Findomain/releases/download/3.0.1/findomain-linux -qO /usr/bin/findomain
 sudo chmod +x /usr/bin/findomain
+
+echo "[+] Installing DalFox (@hahwul)"
+wget -q https://github.com/hahwul/dalfox/releases/download/v2.3.0/dalfox_2.3.0_linux_amd64.tar.gz
+tar -xvf dalfox_2.3.0_linux_amd64.tar.gz
+chmod +x dalfox
+mv dalfox ~/go/bin
 
 echo "[+] Installing Assetfinder (@tomnomnom)"
 go get -u github.com/tomnomnom/assetfinder
@@ -24,11 +32,23 @@ go get -u github.com/tomnomnom/anew
 echo "[+] Installing GF (@tomnomnom)"
 go get -u github.com/tomnomnom/gf
 
+echo "[+] Installing HttpProbe (@tomnomnom)"
+go get -u github.com/tomnomnom/httprobe
+
 echo "[+] Installing Gron (@tomnomnom)"
 go get -u github.com/tomnomnom/gron
 
+echo "[+] Installing Anti Bbroken Url (@tomnomnom)"
+go get -u github.com/tomnomnom/hacks/anti-burl
+
+echo "[+] Installing Comb (@tomnomnom)"
+go get -u github.com/tomnomnom/hacks/comb
+
 echo "[+] Installing QsReplace (@tomnomnom)"
 go get -u github.com/tomnomnom/qsreplace
+
+echo "[+] Installing Unfulr (@tomnomnom)"
+go get -u github.com/tomnomnom/unfurl
 
 echo "[+] Installing GoSpider (@jaeles-project)"
 go get -u github.com/jaeles-project/gospider
@@ -39,16 +59,13 @@ go get -u github.com/hakluke/hakrawler
 echo "[+] Installing Unew (@dwisiswant0)"
 go get -u github.com/dwisiswant0/unew
 
-echo "[+] Installing DalFox (@hahwul)"
-go get -u github.com/hahwul/dalfox
-
 echo "[+] Installing FFuF (@ffuf)"
 go get -u github.com/ffuf/ffuf
 
 echo "[+] Installing Confused (@visma-prodsec)"
 go get -u github.com/visma-prodsec/confused
 
-echo "[+] Installing Crobat (@sonarsearch)"
+echo "[+] Installing Crobat (@cgboal)"
 go get -u github.com/cgboal/sonarsearch/crobat
 
 echo "[+] Installing GAU (@lc)"
@@ -68,6 +85,14 @@ GO111MODULE=on go get -u github.com/projectdiscovery/httpx/cmd/httpx
 
 echo "[+] Installing Naabu (@projectdiscovery)"
 GO111MODULE=on go get -u github.com/projectdiscovery/naabu/v2/cmd/naabu
+
+echo "[+] Installing Chrome Browser (@linux)"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+echo "[+] Installing GoWitness (@sensepost)"
+wget https://github.com/sensepost/gowitness/releases/download/2.3.1/gowitness-2.3.1-linux-amd64 -qO ~/go/bin/gowitness
+chmod +x ~/go/bin/gowitness
 
 echo "[+] Installing SlackTee (@course-hero)"
 git clone https://github.com/course-hero/slacktee.git
